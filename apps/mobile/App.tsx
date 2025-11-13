50ba41daeeb628d937f32ba1a869d08854dbe3b6
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-11-13 18:29:40 -0300

    update Dockerfile/compose

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Vida Abundante mobile!</Text>
+      <Text>Vida Abundante MOBILE!</Text>
       <StatusBar style="auto" />
     </View>
   );
